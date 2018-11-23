e13b6cb16f9bb222e055985505281cb0d7b4ae7e
Esger Jellema <esgerj@gmail.com>
2018-11-23 16:01:03 +0100

    Derde fase nog niet goed
    Animaties worden herstart na verplaatsen element

--- rotate.js
+++ rotate.js
@@ -7,17 +7,41 @@
     // webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend
 
     var count = 0;
+    var halfTime = 5000;
     var yinyang = document.getElementById('Layer_1_1_');
+    var whiteLeft = document.getElementsByClassName('whiteLeft')[0];
+    var whiteRight = document.getElementsByClassName('whiteRight')[0];
+    var blackLeft = document.getElementsByClassName('blackLeft')[0];
+    var blackRight = document.getElementsByClassName('blackRight')[0];
 
-    document.addEventListener("animationiteration", function () {
-        count++;
-        if (count % 2 == 0) {
-            console.log('yo', count);
-            var whiteRight = document.querySelectorAll('whiteRight');
-            var blackRight = document.querySelectorAll('blackRight');
-            yinyang.insertBefore(yinyang.removeChild(whiteRight), blackRight);
+    function onAnimationIteration() {
+        if (count % 4 == 0) {
+            console.log('hele cyclus', count);
+            // blackLeft boven whiteRight
+            yinyang.insertBefore(yinyang.removeChild(whiteRight), blackLeft);
+            setTimeout(function () {
+                console.log('kwart cyclus', count);
+                // blackRight boven whiteRight
+                yinyang.insertBefore(yinyang.removeChild(whiteLeft), blackLeft);
+                yinyang.insertBefore(yinyang.removeChild(whiteRight), blackRight);
+            }, halfTime);
+        } else if (count % 2 == 0) {
+            console.log('halve cyclus', count);
+            yinyang.appendChild(yinyang.removeChild(whiteRight));
+            yinyang.appendChild(yinyang.removeChild(blackRight));
+            setTimeout(function () {
+                console.log('driekwart cyclus', count);
+                yinyang.appendChild(yinyang.removeChild(whiteRight));
+                yinyang.insertBefore(yinyang.removeChild(whiteLeft), blackRight);
+                yinyang.insertBefore(yinyang.removeChild(whiteLeft), blackLeft);
+            }, halfTime);
         }
-    }, true);
+        count++;
+    }
+
+    document.addEventListener("animationiteration", onAnimationIteration, true);
+
+    onAnimationIteration();
 
     // animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd
 
